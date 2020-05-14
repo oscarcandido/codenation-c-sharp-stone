80f2d1a7a8ce9ee879785d83bf769826be7eba6a
Oscar Candido <oscarcandido@me.com>
2020-05-13 21:36:53 -0300

    Ajustes nomes variáveis

--- csharp-0/Source/Math.cs
+++ csharp-0/Source/Math.cs
@@ -7,13 +7,13 @@ namespace Codenation.Challenge
     {
         public List<int> Fibonacci()
         {
-            List<int> FibonacciList = new List<int>() { 0, 1 };
+            List<int> fibonacciList = new List<int>() { 0, 1 };
             do
             {
-                FibonacciList.Add(FibonacciList[FibonacciList.Count - 1] + FibonacciList[FibonacciList.Count - 2]);
+                fibonacciList.Add(fibonacciList[fibonacciList.Count - 1] + fibonacciList[fibonacciList.Count - 2]);
             }
-            while (FibonacciList[FibonacciList.Count - 1] + FibonacciList[FibonacciList.Count - 2] < 350);
-            return FibonacciList;
+            while (fibonacciList[fibonacciList.Count - 1] + fibonacciList[fibonacciList.Count - 2] < 350);
+            return fibonacciList;
         }
 
         public bool IsFibonacci(int numberToTest)
